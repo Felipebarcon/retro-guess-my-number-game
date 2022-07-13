e3bc22b0f0b1911133397c2eb677b441205bfd86
Felipe Barcon <felipe.barcon@gmail.com>
2022-07-13 15:45:27 +0200

    starting dom manipulation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.message').textContent = '🍾 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
